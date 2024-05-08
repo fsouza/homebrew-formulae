1f87d509516ae8b64a778d78ab8c0ffda7689e62
francisco souza <108725+fsouza@users.noreply.github.com>
2024-05-08 11:56:35 -0400

    formula: add tmux@3.3

---
+++ Formula/tmux@3.3.rb
@@ -0,0 +1,88 @@
+class TmuxAT33 < Formula
+  desc "Terminal multiplexer"
+  homepage "https://tmux.github.io/"
+  url "https://github.com/tmux/tmux/releases/download/3.3a/tmux-3.3a.tar.gz"
+  sha256 "e4fd347843bd0772c4f48d6dde625b0b109b7a380ff15db21e97c11a4dcdf93f"
+  license "ISC"
+  revision 1
+
+  livecheck do
+    url :stable
+    regex(/v?(\d+(?:\.\d+)+[a-z]?)/i)
+    strategy :github_latest
+  end
+
+  head do
+    url "https://github.com/tmux/tmux.git", branch: "master"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "libtool" => :build
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "libevent"
+  depends_on "ncurses"
+
+  uses_from_macos "bison" => :build # for yacc
+
+  # Old versions of macOS libc disagree with utf8proc character widths.
+  # https://github.com/tmux/tmux/issues/2223
+  on_system :linux, macos: :sierra_or_newer do
+    depends_on "utf8proc"
+  end
+
+  resource "completion" do
+    url "https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/f5d53239f7658f8e8fbaf02535cc369009c436d6/completions/tmux"
+    sha256 "b5f7bbd78f9790026bbff16fc6e3fe4070d067f58f943e156bd1a8c3c99f6a6f"
+  end
+
+  def install
+    system "sh", "autogen.sh" if build.head?
+
+    args = %W[
+      --enable-sixel
+      --sysconfdir=#{etc}
+    ]
+
+    if OS.mac?
+      # tmux finds the `tmux-256color` terminfo provided by our ncurses
+      # and uses that as the default `TERM`, but this causes issues for
+      # tools that link with the very old ncurses provided by macOS.
+      # https://github.com/Homebrew/homebrew-core/issues/102748
+      args << "--with-TERM=screen-256color"
+      args << "--enable-utf8proc" if MacOS.version >= :high_sierra
+    else
+      args << "--enable-utf8proc"
+    end
+
+    ENV.append "LDFLAGS", "-lresolv"
+    system "./configure", *args, *std_configure_args
+
+    system "make", "install"
+
+    pkgshare.install "example_tmux.conf"
+    bash_completion.install resource("completion")
+  end
+
+  def caveats
+    <<~EOS
+      Example configuration has been installed to:
+        #{opt_pkgshare}
+    EOS
+  end
+
+  test do
+    system bin/"tmux", "-V"
+
+    require "pty"
+
+    socket = testpath/tap.user
+    PTY.spawn bin/"tmux", "-S", socket, "-f", "/dev/null"
+    sleep 10
+
+    assert_predicate socket, :exist?
+    assert_predicate socket, :socket?
+    assert_equal "no server running on #{socket}", shell_output("#{bin}/tmux -S#{socket} list-sessions 2>&1", 1).chomp
+  end
+end
